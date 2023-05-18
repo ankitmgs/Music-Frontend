1f05e79362135f43787ad19d2207ee452dc2b6a4
ankitmgs <85329746+ankitmgs@users.noreply.github.com>
2023-05-18 21:46:10 +0530

    backend link added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-    api_url : 'http://localhost:8000'
+    // api_url : 'http://localhost:8000'
+    api_url : 'https://music-backend-ankit.onrender.com'
 };
 
 export default app_config;
